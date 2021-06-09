36e85ce4d668ba0f295387e58bd0bf1786adcd45
xiaolinzi7118 <1106147743@qq.com>
2021-06-09 15:58:16 +0800

    完成数字输出页面

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,12 +1,13 @@
 import NavLayout from "components/NavLayout"
 import { useState } from "react"
 import { Type } from 'views/Money/Type'
+import { Account } from "./Money/Account"
 
 
 const defaultRecord = {
     id: '0',
     type: 'out',
-    account: 0,
+    account: '0',
     tagId: 1,
     note: '',
     createdAt: ''
@@ -15,11 +16,11 @@ function Money() {
     const [record, setRecord] = useState(defaultRecord)
     const onChange = (obj: Partial<typeof record>) => {
         setRecord({ ...record, ...obj })
-        console.log(record)
     }
     return (
         <NavLayout>
-            <Type value={defaultRecord.type} onChange={(type) => onChange({ type })} />
+            <Type value={record.type} onChange={(type) => onChange({ type })} />
+            <Account value={record.account} type={record.type} />
         </NavLayout>
     )
 }
---
+++ src/views/Money/Account.tsx
@@ -0,0 +1,32 @@
+import classNames from "classnames"
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+.count {
+    font-size: 36px;
+    padding: 9px 16px;
+    text-align: right;
+    height: 72px;
+    &.out {
+        color: green;
+      }
+    &.in {
+        color: red;
+    }
+  }
+`
+type Props = {
+    value: string,
+    type: string
+}
+const Account: React.FC<Props> = (props) => {
+    const { value, type } = props
+    return (
+        <Wrapper>
+            <div className={classNames('count', { 'out': type === 'out' }, { 'in': type === 'in' })}>
+                {value}
+            </div>
+        </Wrapper>
+    )
+}
+export { Account }
